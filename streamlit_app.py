24d7e55d9c2112848396e8d2e2899103a3850d6d
sq01 <256mib@gmail.com>
2022-05-25 18:54:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,7 @@ st.text("ava")
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
-st.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
-st.dataframe(my_fruit_list)
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
+
+fruit_to_show = my_fruit_list.loc[fruits_selected]
+st.dataframe(fruit_to_show)
