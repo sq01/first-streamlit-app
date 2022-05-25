2631e07e4a49424c329780dd33abb53efe67a7bc
sq01 <256mib@gmail.com>
2022-05-25 18:44:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
+import pandas as pd
 import streamlit as st
 
 st.title("Hello World!")
 st.header("Ein, zwei, drei")
 st.text("ava")
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
