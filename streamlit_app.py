2a4e009a9fc5985b05518b2351aa524ef2c8dcb1
sq01 <256mib@gmail.com>
2022-05-25 18:47:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ st.title("Hello World!")
 st.header("Ein, zwei, drei")
 st.text("ava")
 
-st.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
-
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
 st.dataframe(my_fruit_list)
